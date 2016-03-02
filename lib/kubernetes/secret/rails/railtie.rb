aa20813eeae46f9662aa24bb8177c660d3eb5741
Zack Siri <zack@codemy.net>
2016-03-02 13:49:44 +0700

    load before_configuration

--- lib/kubernetes/secret/rails/railtie.rb
+++ lib/kubernetes/secret/rails/railtie.rb
@@ -4,7 +4,7 @@ module Kubernetes
   module Secret
     module Rails
       class Railtie < ::Rails::Railtie        
-        config.after_initialize do 
+        config.before_configuration do 
           ::Rails.application.secrets.merge!({kubernetes: Secret.load_secrets})
         end
       end
